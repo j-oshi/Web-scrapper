96cc1d9398e2c16e2d1cad4e01ff92b717ed59e3
j-oshin <jtoshin@msn.com>
2020-07-21 23:16:12 +0100

    remove printt

--- run_scrapper.php
+++ run_scrapper.php
@@ -2,10 +2,6 @@
 
 include './scrapper.php';
 
-print '<pre>';
-print_r('Please wait, it might take a while....');
-print '</pre>';
-
 $scrapper = new scrapper('https://jobs.sanctuary-group.co.uk/search');
 $scrapper->getFullPageData();
 print '<pre>';
